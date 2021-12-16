05eca6636641ff06bcbdce398f36f9de66e769c2
He Wang <wangheyet@gmail.com>
2021-12-15 22:14:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ dataSelector = [['Dog_1',0,0],
 predictions = []; testSamples = pd.DataFrame()
                 
 for num, dataSet in enumerate(dataSelector):
-    print dataSet, num
+    print (dataSet, num)
     print ("Loading train/validation samples using selector:\n"),dataSelector[num]
     samples = loadTrainAndValidationSamples([dataSet],['allFeats'],100.0)
     print ("Training sample size: "),samples['train'].shape
